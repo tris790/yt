e4283257bbe6d1bd2823eb447b5afab1de4780aa
Tristan Deschamps <tris790@gmail.com>
2021-10-08 21:50:47 -0400

    update

--- browserify-script.js
+++ browserify-script.js
@@ -2,7 +2,6 @@ const ytdl = require('ytdl-core');
 var Stream = require('stream');
 
 async function download(url, options) {
-    videoInfo.videoDetails
     const filename = options.filename || (await ytdl.getInfo(url))?.videoDetails?.title || "download." + options.audioOnly ? "mp3" : "mp4";
 
     let result = new Uint8Array();
